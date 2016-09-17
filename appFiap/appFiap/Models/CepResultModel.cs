54ff95bebc5bd51c9f29729e04ab4e171e144bde
Diogo Koshiba <Guest@L2602MICRO18.local>
2016-09-17 12:07:18 -0300

    cep json

---
+++ appFiap/appFiap/Models/CepResultModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace appFiap
+{
+	public class CepResultModel
+	{
+		public CepResultModel()
+		{
+		}
+
+		public string cep { get; set; }
+
+		[JsonProperty("logradouro")]
+		public string rua { get; set; }
+
+		public string complemento { get; set; }
+
+		public string bairro { get; set; }
+
+		[JsonProperty("localidade")]
+		public string cidade { get; set; }
+
+		[JsonProperty("uf")]
+		public string estado { get; set; }
+	}
+}
+
--- appFiap/appFiap/Pages/AdressPage.xaml.cs
+++ appFiap/appFiap/Pages/AdressPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Newtonsoft.Json;
+using System.Net.Http;
+using Acr.UserDialogs;
 using Xamarin.Forms;
 
 namespace appFiap
@@ -10,6 +12,35 @@ namespace appFiap
 		public AdressPage()
 		{
 			InitializeComponent();
+		}
+
+		async void Handle_Unfocused(object sender, Xamarin.Forms.FocusEventArgs e)
+		{
+			string sURL = "http://viacep.com.br/ws/{0}/json/";
+
+			HttpClient cliente = new HttpClient();
+
+			var uri = new Uri(string.Format(sURL, txtCep.Text));
+			var response = await cliente.GetAsync(uri);
+
+			if (!response.IsSuccessStatusCode)
+			{
+				UserDialogs.Instance.ShowError("erro");
+				return;
+			}
+
+			UserDialogs.Instance.ShowSuccess("sucesso");
+			var content = await response.Content.ReadAsStringAsync();
+
+			CepResultModel cep = new CepResultModel();
+			cep = JsonConvert.DeserializeObject<CepResultModel>(content);
+
+			txtRua.Text = cep.rua;
+			txtBairro.Text = cep.bairro;
+			txtComplemento.Text = cep.complemento;
+			txtCidade.Text = cep.cidade;
+			txtEstado.Text = cep.estado;
+			txtTexto.Focus();
 		}
 	}
 }
